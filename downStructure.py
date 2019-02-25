fbfa01bb54707bd5cf03980358062df391d6c777
LucunJi <36262513+LucunJi@users.noreply.github.com>
2019-02-25 15:12:49 +0800

    Create downStructure.py

---
+++ downStructure.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import os
+
+helpmsg ='''------------
+!!getStructure <URL> -下载结构文到本地
+------------'''
+
+def onServerInfo(server, info):
+  if info.isPlayer == 1:
+    if info.content.startswith('!!pget'):
+      if (len(args) == 1):
+        for i in os.listdir('../'):
+          server.say(i)
