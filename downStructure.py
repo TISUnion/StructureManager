c0ef8809d5267e5a24e4769e31f47e1229178e7c
LucunJi <36262513+LucunJi@users.noreply.github.com>
2019-02-25 15:27:09 +0800

    Update downStructure.py

--- downStructure.py
+++ downStructure.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 
 helpmsg ='''------------
-!!getStructure <URL> -下载结构文到本地
+!!getStructure <URL> -下载结构文件到本地
 ------------'''
 
 def onServerInfo(server, info):
   if info.isPlayer == 1:
     if info.content.startswith('!!getStructure'):
-      if (len(args) == 1):
-        for i in os.listdir('../'):
-          server.say(i)
+      if info.content == '!!getStructure':
+        for line in helpmsg.splitlines():
+          server.tell(info.player, line)
+      elif info.content.startswith == '!!getStructure dir ':
+        for i in info.content.split(' ')[2]:
+          server.tell(info.player, i)
