55e7a8cf2d8cfd99b3045421d11b3686eec75a27
LucunJi <36262513+LucunJi@users.noreply.github.com>
2019-02-26 15:45:09 +0800

    Update downStructure.py

--- downStructure.py
+++ downStructure.py
@@ -10,7 +10,7 @@ helpmsg ='''------------
 文件夹和文件名只兼容英文大小写字母、数字和下划线
 使用 -o 在重名时覆盖
 使用 -b64 打开base64模式，将会作为base64编码的文件被下载和解码
-!!structget -l (<key>) -列出服务器的结构文件
+!!structget -l (<key>) -列出服务器的结构文件，最多一次20个
 指定关键字进一步定位
 ------------'''
 
@@ -31,28 +31,36 @@ def onServerInfo(server, info):
                 args = info.content.split(' ')
                 can_overwrite = info.content.find(' -o') > -1
                 if info.content.find(' -b64') > -1:
-                    getStruct(args[1], args[2], args[3], can_overwrite)
+                    getStructB64(server, args[1], args[2], args[3], can_overwrite)
                 else:
-                    getStructB64(args[1], args[2], args[3], can_overwrite)
+                    getStruct(server, args[1], args[2], args[3], can_overwrite)
   except:
     lines = traceback.format_exc().splitlines()
     for l in lines:
         server.say(l)
                      
 def getStruct(server, foldername, filename, url, can_overwrite):
-#     result = os.system('cd server\world\generated\ && wget -N ' + args[1])
-#         if result == 0:
-#           server.say('success')
-#         else:
-#           server.say('failed to download')
-    pass
+    try:
+        os.mkdir('server/world/generated/'+foldername+'/structures')
+    except:
+        pass
+    result = os.system('cd server/world/generated/'+foldername+'/structures && wget -O '+filename+'.nbt -N '+url)
+    if result == 0:
+        server.say('§a'foldername+':'+filename+'&r has been downloaded successfully')
+    else:
+        server.say('failed to download')
     
 def getStructB64(server, foldername, filename, url, can_overwrite):
     pass
 
 def listStruct(server, player, kwrd=''):
+    counter = 0
     for i in os.listdir('server/world/generated/'):
         for j in os.listdir('server/world/generated/'+i+'/structures'):
             strout = i+':'+j
             if strout.find(kwrd) > -1:
-                server.tell(player, strout)
+                server.tell(player, '§a'+strout[0:len(strout)-4]+'§r')
+
+                counter += 1
+                if counter > 20:
+                    return
