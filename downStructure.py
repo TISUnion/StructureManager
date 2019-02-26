7352c8c5cd8b269aa874c55c0d6447beb8ea3eb1
LucunJi <36262513+LucunJi@users.noreply.github.com>
2019-02-26 16:25:10 +0800

    Update downStructure.py

--- downStructure.py
+++ downStructure.py
@@ -44,11 +44,11 @@ def getStruct(server, foldername, filename, url, can_overwrite):
         os.makedirs('server/world/generated/'+foldername+'/structures')
     except:
         pass
-    result = os.system('cd server/world/generated/'+foldername+'/structures && wget -O '+filename+'.nbt -N '+url)
+    result = os.system('cd server/world/generated/'+foldername+'/structures && wget'+('' if can_overwrite else ' -nc')+' -O '+filename+'.nbt -N '+url)
     if result == 0:
-        server.say('§a'+foldername+':'+filename+'&r has been downloaded successfully')
+        server.say('§a'+foldername+':'+filename+'§r has been downloaded successfully')
     else:
-        server.say('failed to download')
+        server.say('§cfailed to download§r')
     
 def getStructB64(server, foldername, filename, url, can_overwrite):
     pass
